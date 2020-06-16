034581311871622b81ab2c71e9ff2a87ea80dfec
edk52 <edk52@cornell.edu>
2020-06-16 18:15:04 -0400

    work on linked list c implementation

--- C-code/LL.c
+++ C-code/LL.c
@@ -1,3 +1,4 @@
+//Doubly linked list
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -14,31 +15,78 @@ node_t* lastElement(node_t *node){
 	return temp;
 }
 
-void appendLL(node_t *start, node_t *newNode){
+void appendLL(node_t *list, node_t *newNode){
 /*
  * Append new node to end of linked list
- *Return 1 on sucess and 0 if failure
  *
- *start= first node in linked list
+ *list= pointer to linked list
  *newNode= node to append
  * */
-	node_t *lastElem= lastElement(start);
+	node_t *lastElem= lastElement(list);
 	lastElem->next= newNode;
 }
 
+void insertBetween(node_t **list, node_t *nodeBefore, node_t *newNode){
+/*
+Append a new node anywhere in the linked list, after <nodeBefore>
+If <nodeBefore> is NULL, append to list of the list.
+Assumes nodeBefore is a valid node in the same list as node list.
+
+list= pointer to linked list
+nodeBefore= node that we want to append our new node after
+newNode= node to insert
+*/
+	//Insert at front of LL
+	if(nodeBefore == NULL){
+		newNode->next= *list;
+		(*list)->prev= newNode;
+		(*list)= newNode;
+		return;
+	}
+	//insert anywhere else in LL
+	node_t *nodeAfter= nodeBefore->next;
+	nodeBefore->next= newNode;
+	newNode->prev= nodeBefore;
+	newNode->next= nodeAfter;
+	nodeAfter->prev= newNode;
+}
+
+void removeNode(node_t *list , node_t *nodeRemove){
+}
+
+void freeAll(node_t *list){
+/*
+Given a pointer to a linked list, free all nodes in the list
+   */
+	node_t *temp;
+	for(temp= list; temp != NULL; list= temp){
+		temp= list->next;
+		free(list);
+	}
+}
+
+void printAll(node_t *list){
+	node_t *temp;
+	int index= 0;
+	for(temp= list; temp != NULL; temp= temp->next){
+		printf("Node %d: %f\n", index, temp->val);
+		index++;
+	}
+}
 
 int main(){
     node_t *LL= malloc(sizeof(node_t));
-    LL->val= 5.5;
-	node_t *node1= malloc(sizeof(node_t));
-	node1->val=10;
-	appendLL(LL, node1);
-	printf("%f\n", (node1>val));
-
-//	printf("%f\n", (LL->next)->val);
+    LL->val= 1.0;
+	node_t *node2= malloc(sizeof(node_t));
+	node2->val=2.0;
+	//appendLL(LL, node2);
+	insertBetween(&LL, NULL, node2);
+	//printf("First node value: %f\n", LL->val);
+	//printf("Node 2 value: %f\n", (LL->next)->val);
+	//printf("Node 1 value: %f\n", (node2->val));
+	printAll(LL);
     
-	free(LL);
-	free(node1);
+	freeAll(LL);
     return 0;
 }
 
--- C-code/test.c
+++ C-code/test.c
@@ -2,7 +2,11 @@
 int main(){
     //%a.bf specifices a total digits w/ b digits after the decimal point
     float a= 6.546666;
-    printf("%5.4f", a);
+    //printf("%5.4f", a);
+	int x,y;
+	for(x=0, y=0; x <5; x++, y++){
+		printf("x: %d\ny: %d\n\n", x, y);
+	}
 
     return 0;
 }
